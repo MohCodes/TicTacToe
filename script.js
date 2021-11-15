08e7e4147386aee29ce724087490f178315778c6
mo <moefarts@my.yorku.ca>
2021-11-14 19:44:36 -0500

    --all

--- script.js
+++ script.js
@@ -1,88 +1,93 @@
+
+
+
 const gameBoard = (() => {
-    const board = ['square1','square2','square3','square4','square5','square6','square7','square8','square9'];
- return {board};
+    const board = ['','','','','','','','',''];
+    const winningCombination = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],]
+ return {winningCombination,board};
 })();
 
 const players = (name,symbol) => {
     return{ name, symbol};
 };
 
-// const gameFlow = (() => {
-//     const player1 = players('player1','X');
-//     const player2 = players('player2','O');
 
-//     let turn = "X";
-
-//     const nextTurn = ()=>{
-
-//         if(turn =="X"){
-//             turn = "O";
-//         }
-//         else{
-//             turn = "X";
-//         };
-//     };
-
-//     return{player1,player2,turn, nextTurn};
-// })();
-
-
+    //players
+    const player1 = players('player1','X');
+    const player2 = players('player2','O');
+    
 
 const render = (() => {
 
-    const player1 = players('player1','X');
-    const player2 = players('player2','O');
 
-    let turn = "X";
+    //turn changing logic
+    let turn = player1;
 
     const nextTurn = ()=>{
 
-        if(turn =="X"){
-            turn = "O";
+        if(turn ==player1){
+            turn = player2
         }
         else{
-            turn = "X";
+            turn = player1
         };
     };
     
-    const createGridSquares = () =>{ 
+    //creating DOM elements for grid
+
+    const renderUsedSquares =()=>{
+        let mainDiv = document.getElementById("ticTacToeContainer");
+        mainDiv.innerHTML =``;
+
         for (var i = 0; i<gameBoard.board.length;i++){
+            if(gameBoard.board[i] == 'X'||gameBoard.board[i] == 'O'){
             let div = document.createElement('div');
             div.className = `squares`;
-            div.setAttribute('id',`${gameBoard.board[i]}`);
-
-            let mainDiv = document.getElementById("ticTacToeContainer");
-            mainDiv.appendChild(div);
-        };
-    };
-
-    const AddEventListners = () => {
-
-        createGridSquares();
-        for(var i = 0; i<gameBoard.board.length;i++){
-           let div = document.getElementById(gameBoard.board[i]);
-            
-           function addInput(){
-            div.textContent = `${turn}`;
-            if (turn == "X"){
+            div.setAttribute('id',`${i}`);
+            div.textContent = gameBoard.board[i];
+            if(gameBoard.board[i] == "X"){
                 div.style.color = "red"
             }
-            if(turn == "O"){
-                div.style.color = "green"
+            else if (gameBoard.board[i] == "O"){
+                div.style.color = "darkcyan"
             }
-            nextTurn();
-            div.removeEventListener('click',addInput);
-           };
-
-           div.addEventListener('click', addInput)
-           };
+            mainDiv.appendChild(div);
+            div.removeEventListener('click', addInput);
+            }
+            else{
+                let div = document.createElement('div');
+                div.className = `squares`;
+                div.setAttribute('id',`${i}`);
+                div.textContent = gameBoard.board[i];
+                mainDiv.appendChild(div);
+                div.addEventListener('click', addInput);
+            }
         };
 
-    return {AddEventListners};
 
-})();
+    }
 
-render.AddEventListners();
+    //event listeners that add input to grid
+    const addInput = (e) => {
 
+        const squareClicked = e.target.id;
+        gameBoard.board.splice(squareClicked,1,`${turn.symbol}`);
+        nextTurn();
+        renderUsedSquares();
 
+    }
+
+     return {renderUsedSquares,addInput};
+
+    })();
+
+ render.renderUsedSquares();
 
